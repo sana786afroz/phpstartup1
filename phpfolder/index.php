98cb854c923ef3f2605701126fa7268883b16a85
Sana Afroz <119676298+sana786afroz@users.noreply.github.com>
2023-07-15 11:07:46 +0530

    Add files via upload

---
+++ phpfolder/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StartUp with PHP</title>
+</head>
+<body>
+    <div class="container">
+        this is my first php website.
+        <br/>
+        
+        <?php
+        $var1=23;
+        $var2=34;
+        echo "this line is  printed using php command";
+        echo "<br/>";
+        echo "it is used for backend purpose";
+        echo "it is dynamically type language";
+        echo "it is case sensitive ";
+        echo "<br/>";
+        echo "sum of var1 and var2 is ";
+        echo $var1+$var2
+        echo "<br/>";
+
+
+       
+    //     OPERATORS IN PHP
+    //    Airthmatical operators 
+    $num1=35;
+    $num2= 17;
+
+    //    logical operators
+    //    comparision OPERATORS
+    //    assignment operators
+    //    increment/decrement operators 
+        ?>
+    </div>
+</body>
+</html>
